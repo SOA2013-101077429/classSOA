700f79ef44a46c72cf5115259df205d8d335f3f1
saasbook <saasbook@saasbook.(none)>
2013-10-13 07:48:54 -0700

    Updated rails journal and myrottenpotatoes

---
+++ classSOA/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'f717db7a080ec1a0dc2cc3257b6fbe92a17095696d49faeaa96154ef35baf786cf9afa126b028ccfdc29f11028ac9b68cbcc18fde464a40ca8c9752cd7dfb8f4'
